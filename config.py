5dee560df12c15d1feaa42dc87e26391c3cf5679
Federico <f3d3.96@live.it>
2022-07-31 11:55:51 +0200

    Cosmetic fixes

---
+++ config.py
@@ -0,0 +1,8 @@
+# Add your personal MTA API key
+MTA_API_key = "***REMOVED***"
+
+# Add your personal Telegram bot's token.
+BOT_TOKEN = "***REMOVED***"
+
+# Add Telegram IDs of the bot's admins
+LIST_OF_ADMINS = [***REMOVED***]

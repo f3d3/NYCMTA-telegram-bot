82e8ca1cc328cc1d5a7fd3b627487a22402291f2
Federico <f3d3.96@live.it>
2022-08-02 10:29:14 +0200

    Reordered borough tiles and renamed files

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ MTA_API_key = "ymFcaLS9JBabZieClasw2XzdXnedgfE8QxBTUED0"
 # Bot token for local testing: 5457030450:AAE4Xd36ry6w5g3M8cW7cDqGp_S1uYykb5Y
 
 # Add your personal Telegram bot's token.
-BOT_TOKEN = "5457030450:AAE4Xd36ry6w5g3M8cW7cDqGp_S1uYykb5Y"
+BOT_TOKEN = "5471236713:AAFbzrN0_42M3FtMxuPU43XGn5vvzs2zD_U"
 
 # Add Telegram IDs of the bot's admins
 LIST_OF_ADMINS = [13438771]
